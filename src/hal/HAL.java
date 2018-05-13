b8c9079370172646fa8cd6764ed4ccd314b75053
debian <debian@debian>
2018-05-13 20:37:39 +0200

    Alles Paletti!

--- src/hal/HAL.java
+++ src/hal/HAL.java
@@ -11,14 +11,27 @@ import java.io.IOException;
 public class HAL {
 
     public static void main(String[] args) throws FileNotFoundException, IOException {
-        System.out.println("hello halinterpreter");
-        File f = new File("C:\\Users\\ThePhong\\Dropbox\\h_da\\3. Semester\\Betriebssystem\\Praktikum\\HAL\\src\\hal\\HALprogramm.txt");
-        Parser p = new Parser(f);
-        p.lesen();
-        p.show();
-        p.schliessen();
-        p.interpreter();
-
+        
+        for (int i = 0; i < args.length; i++) {
+            System.out.println(args[i]);
+        }
+        if (args[0].equals("0"))
+            System.out.println("Der erste Parameter ist: " + args[0]);
+        else if (args[0].equals("1"))
+            System.out.println("Hallo Welt!");
+        else
+            System.out.println("Der Parameter ist nicht erlaubt!");
     }
+} 
 
-}
+//        System.out.println("hello halinterpreter");
+//        File f = new File("C:\\Users\\ThePhong\\Dropbox\\h_da\\3. Semester\\Betriebssystem\\Praktikum\\HAL\\src\\hal\\HALprogramm.txt");
+//        Parser p = new Parser(f);
+//        p.lesen();
+//        p.show();
+//        p.schliessen();
+//        p.interpreter();
+//
+//    }
+//
+//}
