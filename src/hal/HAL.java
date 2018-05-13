04ba650ccfdefa0ee448f3b87e3b0c9e5ab04b8f
TPN <thephongnguyen@hotmail.de>
2018-05-13 14:07:51 +0200

    Merge origin/master
    Conflicts:
    	src/hal/Hal.java

--- src/hal/HAL.java
+++ src/hal/HAL.java
@@ -12,7 +12,7 @@ public class HAL {
     
     public static void main(String[] args) throws FileNotFoundException, IOException {
         System.out.println("hello halinterpreter");
-        File f = new File("/home/debian/NetBeansProjects/BSpraktikum03/src/hal/HALprogramm.txt");
+        File f = new File("C:\\Users\\ThePhong\\Dropbox\\h_da\\3. Semester\\Betriebssystem\\Praktikum\\HAL\\src\\hal\\HALprogramm.txt");
         Parser p = new Parser(f);
         p.lesen();
         p.show();
