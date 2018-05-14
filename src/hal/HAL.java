2741d4acd0680c1a8965aad1a3a5508848ed70f2
debian <debian@debian>
2018-05-14 15:17:55 +0200

    Merge origin/master
    Conflicts:
    	src/hal/HAL.java

--- src/hal/HAL.java
+++ src/hal/HAL.java
@@ -12,26 +12,25 @@ public class HAL {
 
     public static void main(String[] args) throws FileNotFoundException {
         boolean debugmode = false;
-        File f = new File("/home/debian/Dokumente/Betriebssystem/HAL_interpreter/BSpraktikum03/src/hal/HALprogramm.txt");
-
-        if (args[0].equals("debug")) {
+        File f = new File("empty");
+        
+        if (args[0].equals("-debug")) {
             debugmode = true;
-            Parser p;
-            p = new Parser(f);
-
-            p.lesen();
-            p.show();
-            p.schliessen();
-            p.interpreter(debugmode);
-        } else if (args[0].equals(f)) {
-
-            Parser p;
-            p = new Parser(f);
-
-            p.lesen();
-            p.show();
-            p.schliessen();
-            p.interpreter(debugmode);
+        } else {
+            f = new File("/home/debian/NetBeansProjects/BSpraktikum03/src/HALcodes/" + args[0]);
         }
+        if (args[1].equals("-debug")) {
+            debugmode = true;
+        } else {
+            f = new File("/home/debian/NetBeansProjects/BSpraktikum03/src/HALcodes/" + args[1]);
+        }
+        
+        Parser p;
+        p = new Parser(f);
+        p.lesen();
+        p.show();
+        p.schliessen();
+        p.interpreter(debugmode);
+
     }
 }
